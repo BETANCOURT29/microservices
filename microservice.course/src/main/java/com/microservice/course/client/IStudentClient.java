6d5fcb366db49f364b99652fe15b0d6da72fcea8
Alejandro Betancourt Ballesteros <alejandro.betancourt@pragma.com.co>
2023-12-19 16:37:58 -0500

    primer commit

---
+++ microservice.course/src/main/java/com/microservice/course/client/IStudentClient.java
@@ -0,0 +1,15 @@
+package com.microservice.course.client;
+
+import com.microservice.course.dto.StudentDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "msvc-student", url = "http://localhost:8080/api/student")
+public interface IStudentClient {
+
+    @GetMapping("/search-by-course/{idCourse}")
+    List<StudentDto> findALlStudentsByCourse(@PathVariable Long idCourse);
+}
